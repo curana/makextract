bc8075f3e4ba9aeeeed7249acba6c5ecedde08db
Ben. <curana@users.noreply.github.com>
2013-12-05 22:01:44 +0100

    Update makextract.sh

--- makextract.sh
+++ makextract.sh
@@ -5,15 +5,22 @@
 # DESCRIPTION: Extracts all used make options from installed ports
 #
 
+# Copy /etc/make.conf to the current directory
+cp /etc/make.conf .
+
+# Extract the ports origin via pkg_info
 set ports = `pkg_info | awk '{ system("pkg_info -o -q " $1) }' | awk '{ print("/usr/ports/"$1) }'`
 
+# Extract the used options from each port
 foreach elem ($ports:q)
 	set config = `echo ${elem:q} | awk '{ system("cd "$1 "&& make showconfig") }'`
 	
+	# Skip ports without options
 	if ("x$config" != "x") then
 		echo '.if ${.CURDIR:M*'${elem:q}'/*}' >> make.conf
 	endif
-
+	
+	# Write discovered options to local make.conf
 	foreach elem ($config:q)
 		if ($elem:q =~ *=*:) then
 			set output = `echo ${elem:q} | sed '$s/.$//'` 
@@ -21,6 +28,7 @@ foreach elem ($ports:q)
 		endif
 	end
 
+	# Skip ports without options
 	if ("x$config" != "x") then
 		echo ".endif" >> make.conf
 	endif
